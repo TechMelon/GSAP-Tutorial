c09202be7bd330bedb7ed08deda5e316b4fc1e07
sneha <snehabhardwaj035@gmail.com>
2024-09-24 11:22:57 +0530

    Scroll trigger

---
+++ 2nd/script.js
@@ -0,0 +1,44 @@
+gsap.from("#page1 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    opacity:0,
+    duration:2,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:3,
+        pin:true,
+    },
+})
+gsap.from("#page3 #box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page3 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+    }
+})
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%",
+//     }
+// })
